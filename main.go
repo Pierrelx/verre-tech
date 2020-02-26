35364f97e620150008492ad555cb67be74e6ec58
PierreLx <leroux.pierre.76@gmail.com>
2020-02-26 22:41:04 +0100

    Insertion de store fonctionnelle

--- main.go
+++ main.go
@@ -6,12 +6,26 @@ import (
 
 	db "github.com/PierreLx/verre-tech/services"
 	store "github.com/PierreLx/verre-tech/stores"
+	storeSt "github.com/PierreLx/verre-tech/structs/stores"
 
 	"github.com/go-chi/chi"
 )
 
 func main() {
 	fmt.Println("Démarrage du serveur")
+
+	var storeItem storeSt.Store
+	storeItem.Name = "Store"
+	storeItem.Address = "Ici et là"
+	storeItem.PostalCode = "76000"
+	storeItem.County = "Seine-Maritime"
+	storeItem.City = "City"
+	storeItem.Type = "Classique"
+	storeItem.Latitude = 46.2555
+	storeItem.Longitude = 1.6497
+
+	db.InsertStore(storeItem)
+
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Page d'accueil"))
@@ -20,8 +34,6 @@ func main() {
 	r.Route("/store", func(r chi.Router) {
 		store.HandleRoutes(r)
 	})
-
-	db.Connection()
 
 	http.ListenAndServe(":3000", r)
 }
--- services/database.go
+++ services/database.go
@@ -4,27 +4,54 @@ import (
 	"database/sql"
 	"log"
 
+	storeSt "github.com/PierreLx/verre-tech/structs/stores"
+
 	//Driver pour postgresql (à vérifier)
 	_ "github.com/lib/pq"
 )
 
-func main() {
+const connStr = "user=postgres dbname=TestGolang password=JuVT75XX  host=localhost port=5432 sslmode=disable"
 
+func main() {
+	db, err := Connection()
+
+	err = db.Ping()
+	if err != nil {
+		println("Erreur de connection à la base de données => ", err.Error)
+	}
+
+	defer db.Close()
 }
 
 //Connection permet d'ouvrir la base de données
-func Connection() {
-	connStr := "user=postgres dbname=TestGolang password=JuVT75XX  host=localhost port=5432 sslmode=disable"
+func Connection() (*sql.DB, error) {
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
 
-	rows, err := db.Query("CREATE TABLE TestTable(ID int)")
+	println("Base de données connectée")
+	return db, nil
+}
+
+//InsertStore insère une entité
+func InsertStore(item storeSt.Store) {
+	localDB, err := Connection()
+
+	query, err := localDB.Prepare("INSERT INTO Stores(Name, Address, PostalCode, County, City, Type, Latitude, Longitude) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)")
 	if err != nil {
-		log.Fatal(err)
+		println("Erreur lors de la préparation de la requête")
+		panic(err.Error())
 	}
-	if err == nil {
-		print(rows)
+
+	defer query.Close()
+
+	res, err := query.Exec(item.Name, item.Address, item.PostalCode, item.County, item.City, item.Type, item.Latitude, item.Longitude)
+	if err != nil {
+		println("Erreur lors de l'exécution de la requête")
+		panic(err.Error())
 	}
+	print(res)
 }
